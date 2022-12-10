8f110d46a49a62e25f37253057a6d8cf7c61571b
zoeanderson <zvann813@gmail.com>
2022-12-10 11:37:07 -0700

    commit tree 09cf5b7b1766dfaabc2a064099b305d3a88b4a6b

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Code your solution here
+const drivers = [
+{
+    name: 'Adam',
+    hometown: 'Albany'
+},
+{
+    name: 'Bob',
+    hometown: 'Bosie',
+},
+{
+    name: 'Cassie',
+    hometown: 'Chicago',
+},
+{
+    name: 'Dylan',
+    hometown: 'Denver',
+},
+{
+    name: 'Ellie',
+    hometown: 'Eugene',
+},
+{
+    name: 'Frankie',
+    hometown: 'Fort Worth',
+},
+];
+
+function findMatching(drivers, attribute) {
+    return drivers.filter(function(driver){
+        return driver.toLowerCase() === attribute.toLowerCase()
+    }
+    )
+
+}
+
+function fuzzyMatch(drivers, attribute){
+    return drivers.filter(function (driver) {
+        return driver.toLowerCase().substring(0, attribute.length) === attribute.toLowerCase()
+
+    })
+}
+
+function matchName(drivers, argument){
+    return drivers.filter(function(driver){
+        return driver.name === argument
+    })
+
+}
